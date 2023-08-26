5d1a32f73448412fb579298ce5fdf8ab774a9a3d
ches7 <chesmon1@hotmail.co.uk>
2023-08-26 13:05:12 +0100

    dashboard styles

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,66 +1,93 @@
 <x-app-layout>
 
+  <div class="flex justify-center">
+    
+    <div class="flex flex-col w-1/2">
+
+
+      @unless (count($posts) == 0)
+
+      <div class="flex mt-4 justify-center">
+      Your posts
+      </div>
+
+      <ul role="list" class="divide-y divide-gray-100">
+
+          @foreach ($posts as $post)
+              <li class="flex justify-between gap-x-6 py-5">
+                  <div class="flex min-w-0 gap-x-4">
+                      <div class="min-w-0 flex-auto">
+                          <a href="/posts/{{ $post->id }}">
+                              <p class="text-sm font-semibold leading-6 text-gray-900">{{ $post->title }}</p>
+                          </a>
+                          {{-- <a href="/users/{{ $post->user_id }}">
+                              <p class="mt-1 truncate text-xs leading-5 text-gray-500">{{ $post->user->name }}</p>
+                          </a> --}}
+                      </div>
+                  </div>
+                  <div class="hidden shrink-0 sm:flex sm:items-end mr-2">
+
+                    <x-heroicon-o-chat-bubble-left class="w-6 h-6 mb-0.5"/>
+                    
+                <p class="text-sm leading-6 text-gray-900 pl-1 mr-2 mb-1">{{ count($post->replies) }}</p>
+
+                    <a href="/posts/{{ $post->id }}/edit">
+                        <x-heroicon-o-pencil-square class="w-6 h-6 mb-1.5 mr-1"/>
+                    </a>
+        
+
+                    <form method="POST" action="/posts/{{ $post->id }}">
+                        @csrf
+                        @method('DELETE')
+                        <button><x-heroicon-o-trash class="w-6 h-6 mr-1 text-red-500"/></button>
+                    </form>
+
+                </div>
+              </li>
+          @endforeach
+
+      </ul>
+
+      @else
+<div class="flex mt-4 justify-center">
+  No posts
+  </div>
+  @endunless
+
+
+  @unless (count($users) == 0)
+
+  <div class="flex mt-4 justify-center">
   Your messages
+  </div>
 
-  Your posts
+  <ul role="list" class="divide-y divide-gray-100">
 
-    <table class="w-full table-auto rounded-sm">
-        <tbody>
-          @unless($posts->isEmpty())
-          @foreach($posts as $post)
-          <tr class="border-gray-300">
-            <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
-              <a href="/posts/{{$post->id}}"> {{$post->title}} </a>
-            </td>
-            <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
-              <a href="/posts/{{$post->id}}/edit" class="text-blue-400 px-6 py-2 rounded-xl"><i
-                  class="fa-solid fa-pen-to-square"></i>
-                Edit</a>
-            </td>
-            <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
-              <form method="POST" action="/posts/{{$post->id}}">
-                @csrf
-                @method('DELETE')
-                <button class="text-red-500"><i class="fa-solid fa-trash"></i> Delete</button>
-              </form>
-            </td>
-          </tr>
-          @endforeach
-          @else
-          <tr class="border-gray-300">
-            <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
-              <p class="text-center">No Posts Found</p>
-            </td>
-          </tr>
-          @endunless
-  
-        </tbody>
-      </table>
+      @foreach ($users as $user)
+          <li class="flex justify-between gap-x-6 py-5">
+              <div class="flex min-w-0 gap-x-4">
+                      <a href="/users/{{ $user->id }}">
+                          <p class="text-sm font-semibold leading-6 mt-2">{{ $user->name }}</p>
+                      </a>
+                  </div>
+              <div class="mr-2">
 
+                <a href="/messages/{{ $user->id }}" class="">
+                  <x-heroicon-o-chat-bubble-left-right class="w-6 h-6 mb-1.5 mr-1"/>
+                </a>
 
-      <table class="w-full table-auto rounded-sm">
-        <tbody>
-          @unless($users->isEmpty())
-          @foreach($users as $user)
-          <tr class="border-gray-300">
-            <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
-              <a href="/users/{{$user->id}}"> {{$user->name}} </a>
-            </td>
-            <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
-              <a href="/messages/{{$user->id}}" class="text-blue-400 px-6 py-2 rounded-xl"><i
-                  class="fa-solid fa-pen-to-square"></i>
-                Message</a>
-            </td>
-          </tr>
-          @endforeach
-          @else
-          <tr class="border-gray-300">
-            <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
-              <p class="text-center">No Messages Found</p>
-            </td>
-          </tr>
-          @endunless
-  
-        </tbody>
-      </table>
+            </div>
+          </li>
+      @endforeach
+
+  </ul>
+@else
+<div class="flex mt-4 justify-center">
+  No messages
+  </div>
+
+@endunless
+
+    </div>
+  </div>
 </x-app-layout>
